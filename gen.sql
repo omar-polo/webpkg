035f490ae6ad33992de0d2b5007adbfb4e83ec07
Omar Polo <op@omarpolo.com>
2020-11-06 19:31:42 +0100

    remove temp query

--- gen.sql
+++ gen.sql
@@ -11,12 +11,3 @@ select pathid,
        descr_contents,
        maintainer
   from portsq;
-
-select webpkg_fts.pkgstem,
-       webpkg_fts.comment,
-       p.fullpkgname,
-       paths.fullpkgpath
-  from webpkg_fts
-	 join _ports p on p.fullpkgpath = webpkg_fts.id
-         join _paths paths on paths.id = webpkg_fts.id
- where webpkg_fts match 'godot';
